5a711d694e6cfd75daaf9f19672e11a12c20bb75
dohashehataa <dohashehata7@gmail.com>
2024-07-20 10:26:13 +0300

    updted

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,21 +1,3 @@
-// import { connect } from 'mongoose';
-
-// const MONGO_URI = 'mongodb://localhost:27017/AppJop'; 
-
-// const dbConnection = async () => {
-//   try {
-//     await connect(MONGO_URI);
-//     console.log('MongoDB connected');
-//   } catch (error) {
-//     console.error(error.message);
-//     process.exit(1);
-//   }
-// };
-
-// export default dbConnection;
-
-
-
 
 import { connect } from 'mongoose';
 import dotenv from 'dotenv';
